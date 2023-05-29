4f7710dd018ec131e93b8b8c85a213ddc0df781c
Juan Jesús <122260827+JuanJesusAlejoSillero@users.noreply.github.com>
2023-05-29 06:22:20 +0200

    Test TF

--- Terraform/bucket-s3.tf
+++ Terraform/bucket-s3.tf
@@ -29,22 +29,42 @@ resource "aws_s3_bucket_public_access_block" "bucket-s3-public" {
   restrict_public_buckets = false
 }
 
-data "aws_iam_policy_document" "bucket-s3-policy" {
-  statement {
-    principals {
-      type        = "*"
-      identifiers = ["*"]
-    }
-    actions = ["s3:GetObject"]
-    resources = [
-      "${aws_s3_bucket.bucket-s3.arn}/*"
-    ]
-    sid = "PublicReadGetObject"
-    effect = "Allow"
-  }
-}
+#data "aws_iam_policy_document" "bucket-s3-policy" {
+#  statement {
+#    principals {
+#      type        = "*"
+#      identifiers = ["*"]
+#    }
+#    actions = ["s3:GetObject"]
+#    resources = [
+#      "${aws_s3_bucket.bucket-s3.arn}/*"
+#    ]
+#    sid = "PublicReadGetObject"
+#    effect = "Allow"
+#  }
+#}
+#
+#resource "aws_s3_bucket_policy" "bucket-s3-policy" {
+#  bucket = aws_s3_bucket.bucket-s3.id
+#  policy = data.aws_iam_policy_document.bucket-s3-policy.json
+#}
 
 resource "aws_s3_bucket_policy" "bucket-s3-policy" {
   bucket = aws_s3_bucket.bucket-s3.id
-  policy = data.aws_iam_policy_document.bucket-s3-policy.json
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid = "PublicReadGetObject"
+        Effect = "Allow"
+        Principal = "*"
+        Action = [
+          "s3:GetObject"
+        ]
+        Resource = [
+          "${aws_s3_bucket.bucket-s3.arn}/*"
+        ]
+      }
+    ]
+  })
 }
