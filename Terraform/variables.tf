c74759d4790950041607562e90c42ea195b28423
Juan Jesús <122260827+JuanJesusAlejoSillero@users.noreply.github.com>
2023-05-29 04:47:24 +0200

    Ajustes credenciales

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -20,14 +20,6 @@ variable "aws_region" {
   default = "us-east-1"
 }
 
-variable "aws_access_key_id" {
-  type = string
-}
-
-variable "aws_secret_access_key" {
-  type = string
-}
-
 variable "domain" {
   type = string
   default = "www.juanje.eu"
